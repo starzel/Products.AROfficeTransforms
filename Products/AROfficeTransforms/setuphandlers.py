727d58b01b29234b2416817e82dc97d677df2ceb
f10w <f10w>
2011-04-26 15:26:19 +0000

    Add a flag in setuphandlers to run install import step only when necessary

--- Products/AROfficeTransforms/setuphandlers.py
+++ Products/AROfficeTransforms/setuphandlers.py
@@ -5,6 +5,13 @@ from Products.AROfficeTransforms.config import TRANSFORMS
 
 
 def install(self):
+    # Ordinarily, GenericSetup handlers check for the existence of XML files.
+    # Here, we are not parsing an XML file, but we use this text file as a
+    # flag to check that we actually meant for this import step to be run.
+    # The file is found in profiles/default.
+    if self.readDataFile('Products.AROfficeTransforms-tranforms.txt') is None:
+        return
+    #
     transform_tool = getToolByName(self, 'portal_transforms')
     for transform in TRANSFORMS:
       print "atReal : Adding %s transform" % transform
