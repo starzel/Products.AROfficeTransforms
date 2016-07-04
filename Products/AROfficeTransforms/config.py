1835adc534faca2a26b19f0bc2cf4a21313092bd
Philip Bauer <bauer@starzel.de>
2016-07-04 12:37:45 +0200

    reset config to original values

--- Products/AROfficeTransforms/config.py
+++ Products/AROfficeTransforms/config.py
@@ -1,18 +1,18 @@
 PROJECTNAME = 'AROfficeTransforms'
 
 TRANSFORMS = [
-    # "word_to_text",
-    # "word_to_html",
+    "word_to_text",
+    "word_to_html",
     "excel_to_html",
-    # "ppt_to_html",
-    # "ooo_to_html",
-    # "oo2_to_html",
+    "ppt_to_html",
+    "ooo_to_html",
+    "oo2_to_html",
     "pdf_to_html",
-    # "zip_to_text",
+    "zip_to_text",
 ]
 
 TIMEOUT = {
-    'WARNTIME': 10,
+    'WARNTIME': 120,
     'KILLTIME': 10,
 }
 
